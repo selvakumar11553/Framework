b330a0feae7482694e1689bdb911591b0f5dff69
selvakumar11553 <selvavasanth11553@gmail.com>
2021-12-21 13:41:29 +0530

    done coding

--- src/org/class1/Class.java
+++ src/org/class1/Class.java
@@ -1,5 +1,24 @@
 package org.class1;
 
-public class Class {
+import java.util.ArrayList;
+import java.util.List;
 
-}
+public class Class {
+	public static void main(String[] args) {
+		List<Integer> l=new ArrayList<>();
+		l.add(10);
+		l.add(20);
+		l.add(30);
+		l.add(90);
+		l.add(10);
+		l.add(10);
+		l.add(40);
+		l.add(50);
+		l.add(60);
+		int s= l.lastIndexOf(10);
+		System.out.println("Index last value of 10:"+s);
+		}
+	}
+	
+	
+
